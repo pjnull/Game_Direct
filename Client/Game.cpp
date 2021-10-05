09b3aa3559808c8674af8065df8572a32740ccff
Your Name <you@example.com>
2021-10-05 13:41:01 +0900

    사각형 그리기

--- Client/Game.cpp
+++ Client/Game.cpp
@@ -9,14 +9,52 @@ shared_ptr<Shader>shader = make_shared<Shader>();
 void Game::Init(const WindowInfo& windowinfo)
 {
 	GEngine->Init(windowinfo);
-	vector<Vertex> vec(3);
+	/*vector<Vertex> vec(3);
 	vec[0].pos = Vec3(0.f, 0.5f, 0.5f);
 	vec[0].color = Vec4(1.f, 0.f, 0.f, 1.f);
 	vec[1].pos = Vec3(0.5f, -0.5f, 0.5f);
 	vec[1].color = Vec4(0.f, 1.0f, 0.f, 1.f);
 	vec[2].pos = Vec3(-0.5f, -0.5f, 0.5f);
 	vec[2].color = Vec4(0.f, 0.f, 1.f, 1.f);
-	mesh->Init(vec);
+	*/
+	/*vector<Vertex> vec(6);
+vec[0].pos = Vec3(-0.5f, 0.5f, 0.5f);
+vec[0].color = Vec4(1.f, 0.f, 0.f, 1.f);
+vec[1].pos = Vec3(0.5f, 0.5f, 0.5f);
+vec[1].color = Vec4(0.f, 1.f, 0.f, 1.f);
+vec[2].pos = Vec3(0.5f, -0.5f, 0.5f);
+vec[2].color = Vec4(0.f, 0.f, 1.f, 1.f);
+
+vec[3].pos = Vec3(0.5f, -0.5f, 0.5f);
+vec[3].color = Vec4(0.f, 0.f, 1.f, 1.f);
+vec[4].pos = Vec3(-0.5f, -0.5f, 0.5f);
+vec[4].color = Vec4(0.f, 1.f, 0.f, 1.f);
+vec[5].pos = Vec3(-0.5f, 0.5f, 0.5f);
+vec[5].color = Vec4(1.f, 0.f, 0.f, 1.f);
+	*/
+	vector<Vertex> vec(4);
+	vec[0].pos = Vec3(-0.5f, 0.5f, 0.5f);
+	vec[0].color = Vec4(1.f, 0.f, 0.f, 1.f);
+	vec[1].pos = Vec3(0.5f, 0.5f, 0.5f);
+	vec[1].color = Vec4(0.f, 1.f, 0.f, 1.f);
+	vec[2].pos = Vec3(0.5f, -0.5f, 0.5f);
+	vec[2].color = Vec4(0.f, 0.f, 1.f, 1.f);
+	vec[3].pos = Vec3(-0.5f, -0.5f, 0.5f);
+	vec[3].color = Vec4(0.f, 1.f, 0.f, 1.f);
+
+	vector<uint32> indexVec;
+	{
+		indexVec.push_back(0);
+		indexVec.push_back(1);
+		indexVec.push_back(2);
+	}
+	{
+		indexVec.push_back(0);
+		indexVec.push_back(2);
+		indexVec.push_back(3);
+	}
+
+	mesh->Init(vec, indexVec);
 
 	shader->Init(L"..\\Resources\\Shader\\default.hlsli");
 
@@ -40,7 +78,6 @@ void Game::Update()
 		mesh->Render();
 	}
 
-	mesh->Render();
 	GEngine->RenderEnd();
 
 }
